c468bca837219a1b1669d6bd8f4adaab51270dd5
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-05 22:25:06 -0600

    remove pesky overworld tags who needs em

--- kubejs/server_scripts/worldgen/tags.js
+++ kubejs/server_scripts/worldgen/tags.js
@@ -1,11 +1,11 @@
 ServerEvents.tags('worldgen/biome', e=> {
-    e.add('minecraft:is_overworld', [
-        'splendid_ranching:ancient_ruins',
-        'splendid_ranching:dry_reef',
-        'splendid_ranching:indigo_quarry',
-        'splendid_ranching:magma_mounts',
-        'splendid_ranching:moss_blanket',
-        'splendid_ranching:opal_desert',
-        'splendid_ranching:the_wilds'
-    ])
+    // e.add('minecraft:is_overworld', [
+    //     'splendid_ranching:ancient_ruins',
+    //     'splendid_ranching:dry_reef',
+    //     'splendid_ranching:indigo_quarry',
+    //     'splendid_ranching:magma_mounts',
+    //     'splendid_ranching:moss_blanket',
+    //     'splendid_ranching:opal_desert',
+    //     'splendid_ranching:the_wilds'
+    // ])
 })
