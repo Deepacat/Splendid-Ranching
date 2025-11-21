fefc5eef9ea130cfbc3326d7ee1bf4fff9aeb228
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-20 22:51:50 -0600

    recipe category & biome lang

--- kubejs/client_scripts/lang.js
+++ kubejs/client_scripts/lang.js
@@ -1,7 +1,18 @@
 ClientEvents.lang('en_us', e => {
+    // mbd2 machine
     e.add('block.mbd2.selling_port', 'Sales Terminal')
     e.add('block.mbd2.auto_selling_port', 'Automatic Sales Terminal')
     e.add('block.mbd2.chicken_duper', 'Chicken Duplicator')
     e.add('block.mbd2.slime_breeder', 'Slime Fusion Breeder')
     e.add('block.mbd2.splendid_generator', 'Splendid Generator')
+    // mbd2 recipe categories
+    e.add('mbd2.fake_slime_breeding', 'Slime Fusion Breeding')
+    e.add('mbd2.plort_selling', 'Plort Selling')
+    // biomes
+    e.add('biome.splendid_ranching.dry_reef', 'Dry Reef')
+    e.add('biome.splendid_ranching.indigo_quarry', 'Indigo Quarry')
+    e.add('biome.splendid_ranching.magma_mounts', 'Magma Mounts')
+    e.add('biome.splendid_ranching.moss_blanket', 'Moss Blanket')
+    e.add('biome.splendid_ranching.opal_desert', 'Opal Desert')
+    e.add('biome.splendid_ranching.slimey_sea', 'Slimey Sea')
 })
