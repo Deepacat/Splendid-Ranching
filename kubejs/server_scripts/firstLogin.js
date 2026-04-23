2bfce6f4b252105ee1873b148cff50b00feab3f4
Deepa <52133154+Deepacat@users.noreply.github.com>
2026-04-22 20:56:32 -0500

    Bank card was moved to quest rewards

--- kubejs/server_scripts/firstLogin.js
+++ kubejs/server_scripts/firstLogin.js
@@ -3,7 +3,6 @@ PlayerEvents.loggedIn(e => {
         e.player.give('splendid_slimes:slime_vac')
         e.player.give('ftbquests:book')
         e.player.give('numismatics_utils:bank_meter')
-        e.player.give('numismatics:magenta_card')
 
         e.addGameStage('started')
     }
