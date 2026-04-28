f1c8734c8cc74a886d795d7c40212d69ab604e97
Deepa <52133154+Deepacat@users.noreply.github.com>
2026-04-28 05:32:22 -0500

    Move backpack tiers to nukelist

--- kubejs/server_scripts/mods/sophisticatedbackpacks/removals.js
+++ kubejs/server_scripts/mods/sophisticatedbackpacks/removals.js
@@ -3,21 +3,22 @@
 ServerEvents.recipes(e => {
     e.remove({
         output: [
-            'sophisticatedbackpacks:backpack',
-            'sophisticatedbackpacks:restock_upgrade',
-            'sophisticatedbackpacks:deposit_upgrade',
-            'sophisticatedbackpacks:filter_upgrade',
-            'sophisticatedbackpacks:void_upgrade',
-            'sophisticatedbackpacks:pickup_upgrade',
-            'sophisticatedbackpacks:battery_upgrade',
-            'sophisticatedbackpacks:feeding_upgrade',
+            "sophisticatedbackpacks:backpack",
+            "sophisticatedbackpacks:restock_upgrade",
+            "sophisticatedbackpacks:deposit_upgrade",
+            "sophisticatedbackpacks:filter_upgrade",
+            "sophisticatedbackpacks:void_upgrade",
+            "sophisticatedbackpacks:pickup_upgrade",
+            "sophisticatedbackpacks:battery_upgrade",
+            "sophisticatedbackpacks:feeding_upgrade"
+        ]
+    })
 
-        ]
-    })
-    e.remove({
-        id: [
-            'sophisticatedbackpacks:magnet_upgrade',
-            'sophisticatedbackpacks:advanced_magnet_upgrade'
-        ]
-    })
+    let recipeIdList = [
+        "sophisticatedbackpacks:magnet_upgrade",
+        "sophisticatedbackpacks:advanced_magnet_upgrade"
+    ]
+    for (let recipeId of recipeIdList) {
+        e.remove({ id: recipeId }) // Can't just remove an array for some reason
+    }
 })
