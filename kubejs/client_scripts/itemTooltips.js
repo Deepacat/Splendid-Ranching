5b3315faf69a5767f8f33279c386ce79193413e5
Deepa <52133154+Deepacat@users.noreply.github.com>
2026-04-28 06:51:00 -0500

    lots of tooltip tweaks

--- kubejs/client_scripts/itemTooltips.js
+++ kubejs/client_scripts/itemTooltips.js
@@ -1,41 +1,45 @@
 ItemEvents.tooltip(e => {
     // mbd2
-    e.add('mbd2:selling_port', ["§8[§7 Sells plorts using the interface §8]"])
-    e.add('mbd2:auto_selling_port', [
+    e.add("mbd2:selling_port", ["§8[§7 Sells plorts using the interface §8]"])
+    e.add("mbd2:auto_selling_port", [
         "§8[§7 Automatically sells plorts inside it with a cooldown of 5 minutes §8]",
-        "§8[§4 Drops the currency from the machines face §8]"
+        "§8[§7 Drops the currency from the machines face §8]"
     ])
-    e.add('mbd2:slime_breeder', [
+    e.add("mbd2:slime_breeder", [
         "§8[§7 Fuses slimes in a 5x4x5 area behind it §8]",
         "§8[§7 The front has the eyes, the red dot faces behind §8]",
     ])
-    e.add('mbd2:chicken_duper', [
+    e.add("mbd2:chicken_duper", [
         "§8[§7 50/50 Duplicates or kills a nearby chicken §8]"
     ])
-    e.add('mbd2:splendid_generator', [
+    e.add("mbd2:splendid_generator", [
         "§8[§7 Generates 2.5k FE/gip from your bank account ]",
         "§8[§7 up to 25k FE/second §8]"
     ])
     // splendid slimes
-    e.add('splendid_slimes:corral_block', [
+    e.add("splendid_slimes:corral_block", [
         "§8[§7 Allows players to pass through, but not slimes §8]",
         "§8[§7 Redstone power will disable other mobs passing through §8]"
     ])
-    e.add('splendid_slimes:slime_incubator', ["§8[§7 Grows a slime heart to spawn a new slime §8]"])
-    e.add('splendid_slimes:plort_press', ["§8[§7 Creates slime hearts from plorts §8]"])
-    e.add('splendid_slimes:plort_rippit', ["§8[§7 Extracts resources from a slime heart §8]"])
-    e.add('splendid_slimes:slime_feeder', ["§8[§7 Nearby slimes can eat from it\'s inventory §8]"])
+    e.add("splendid_slimes:slime_incubator", [
+        "§8[§7 Incubates a slime heart into a new slime §8]",
+        "§8[§7 Slime spawns infront of the incubator §8]"
+    ])
+    e.add("splendid_slimes:plort_press", ["§8[§7 Creates slime hearts from plorts §8]"])
+    e.add("splendid_slimes:plort_rippit", ["§8[§7 Extracts resources from plorts §8]"])
+    e.add("splendid_slimes:slime_feeder", ["§8[§7 Feeds slimes in a ~6³ area §8]"])
+    e.add("splendid_slimes:rocket_pod", ["§8[§7 Sends you flying without damage! §8]", "§8[§7 Recommended to use with Rainbounce boots §8]"])
     // quark
-    e.add('quark:feeding_trough', ["§8[§7 Feeds nearby animals from items inside §8]"])
+    e.add("quark:feeding_trough", ["§8[§7 Nearby animals may eat from it §8]"])
     // torchmaster
-    e.add('torchmaster:dreadlamp', ["§8[§7 Slimes count to be blocked from spawning §8]"])
+    e.add("torchmaster:dreadlamp", ["§8[§7 Slimes count to be blocked from spawning §8]"])
     // Farmers delight
-    e.add('farmersdelight:tomato', [
+    e.add("farmersdelight:tomato", [
         "§8[§7 It's a fruit!!! §8]",
         "§8[§7 Marked as a vegetable for culinary purposes §8]"
     ])
     // Numismatics
-    e.add('numismatics_utils:bank_meter', "§8[§7 Displays your bank balance on the top left of your HUD §8]")
+    e.add("numismatics_utils:bank_meter", "§8[§7 Displays your bank balance on the top left of your HUD §8]")
     e.add(/numismatics:(?!.*_id_).+_card/, [
         "§8[§7 Place card into Blaze Banker to link the card to the Bankers account §8]",
         "§8[§7 Equip the linked card in Curios to link yourself to the account §8]",
@@ -44,27 +48,31 @@ ItemEvents.tooltip(e => {
     e.add(/numismatics:\w+_id_card/, [
         "§8[§7 Place others owned ID Cards into your Blaze Banker to allow them access §8]",
     ])
-    e.add('numismatics:spur', "§8[§b I'm just a little guy §8]")
-    e.add('numismatics:bevel', "§8[§5 Looking good today! §8]")
-    e.add('numismatics:sprocket', "§8[§a What am I made of? §8]")
-    e.add('numismatics:cog', "§8[§c I guess you can spend me :| §8]")
-    e.add('numismatics:crown', "§8[§6 What're you lookin' at :P §8]")
-    e.add('numismatics:sun', "§8[§d Mornin' Rancher :) §8]")
+    e.add("numismatics:spur", "§8[§b I'm just a little guy §8]")
+    e.add("numismatics:bevel", "§8[§5 Looking good today! §8]")
+    e.add("numismatics:sprocket", "§8[§a What am I made of? §8]")
+    e.add("numismatics:cog", "§8[§c I guess you can spend me :| §8]")
+    e.add("numismatics:crown", "§8[§6 What're you lookin' at :P §8]")
+    e.add("numismatics:sun", "§8[§d Mornin' Rancher :) §8]")
     // Vanilla
-    e.add('minecraft:flint_and_steel', ["§8[§7 Portals may be created, however", "§cthe nether is disabled! §8]"])
+    e.add("minecraft:flint_and_steel", ["§8[§7 Portals may be created, however", "§cthe nether is disabled! §8]"])
     // Create
-    e.add('create:blaze_burner', '§8[§7 Blazes can be created with Fel Pumpkins §8]')
-    e.add('create:chromatic_compound', [
+    e.add("create:blaze_burner", "§8[§7 Blazes can be created with Fel Pumpkins §8]")
+    e.add("create:chromatic_compound", [
         "§7- At the touch of a beacons beam, ", "§7becomes infused with §rradiance",
         "§7- In the dark of the voids despair, ", "§7becomes infused with §8shadows"
     ])
+    // Create additions
+    e.add("createaddition:tesla_coil", ['§8[§7 Can charge items on a depot with energy §8]'])
     // Botania
-    e.add('botania:fel_pumpkin', [
+    e.add("botania:fel_pumpkin", [
         "§8[§7 Place on top of 2 iron bars to summon a blaze §8]",
         "§8[§7 Summoned blaze only drops powder §8]"
     ])
-    e.add('create:refined_radiance', "§7Forged by a §rgreat light.")
-    e.add('create:shadow_steel', "§7Forged by a §8dark void.")
+    e.add("create:refined_radiance", "§7Forged by a §rgreat light.")
+    e.add("create:shadow_steel", "§7Forged by a §8dark void.")
     // Alexscaves
-    e.add('alexscaves:acid_bucket', "§7An §cexplosion§7 in this could probably destroy... §8anything§7!.")
+    e.add("alexscaves:acid_bucket", "§7An §cexplosion§7 in this could probably destroy... §8anything§7!.")
+    e.add("alexscaves:candy_cane_hook", ["§8[§7 Pulls you to the hooks location §8]", "§8[§7 Enchantable for much more power §8]"])
+    e.add("alexscaves:rainbounce_boots", ["§8[§7 Prevents fall damage and lets you bounce §8]", "§8[§7 Might want to muffle the sound in your inventory... §8]"])
 })
