a5d0662ca6dccee749e62653cf6b787e13600ecf
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2026-05-03 15:53:59 -0500

    Quark Crate Recipe
    It normally only loads the recipe if oddities is enabled.

---
+++ kubejs/server_scripts/mods/quark/shaped.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(e => {
+    e.shaped('quark:crate', ['IPI', 'PCP', 'IPI'], { I: 'minecraft:iron_ingot', P: '#minecraft:planks', C: '#forge:chests/wooden' })
+        .id(`kubejs:quark/shaped/crate`)
+})
