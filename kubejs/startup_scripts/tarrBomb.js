1108594ab3470de7fabea889d7485e8219525c8a
Deepa <52133154+Deepacat@users.noreply.github.com>
2026-08-16 01:01:42 -0500

    tarr bombs with wip assets

--- kubejs/startup_scripts/tarrBomb.js
+++ kubejs/startup_scripts/tarrBomb.js
@@ -12,21 +12,34 @@
 // Throwable bomb projectile for purifying tarrs
 
 /** @param {Internal.ContextUtils$ProjectileEntityHitContext} context */
-global.projHit = (context) => {
+global.projHit = (context, radius) => {
     const { entity, result } = context
     if (entity.level.isClientSide()) return
     const rX = result.location.x()
     const rY = result.location.y()
     const rZ = result.location.z()
 
-    // explosion effects
-    entity.level.spawnParticles(`supplementaries:bomb_explosion`, false, rX, rY + 0.25, rZ, 1, 1, 1, 10, 0)
-    entity.level.spawnParticles(`alexscaves:big_splash`, false, rX, rY + 0.25, rZ, 2.5, 0, 2.5, 25, 0)
-    entity.level.spawnParticles(`alexscaves:bio_pop`, false, rX, rY + 0.25, rZ, 2.5, 0, 2.5, 25, 0)
-    entity.level.spawnParticles(`alexscaves:big_splash_effect`, false, rX, rY + 0.25, rZ, 2.5, 0, 2.5, 50, 0)
-    entity.level.playSound(null, rX, rY, rZ, "alexscaves:mine_guardian_explode", "players", 1, Math.random() * (2 - 1.6) + 1.6)
+    // explosion visual effects
+    entity.level.spawnParticles(`supplementaries:bomb_explosion`,
+        false, rX, rY + 0.25, rZ,
+        radius / 8, 1, radius / 8,
+        radius * 2, 0)
+    entity.level.spawnParticles(`alexscaves:big_splash`,
+        false, rX, rY + 0.25, rZ,
+        radius / 3, 0, radius / 3,
+        radius * 4, 0)
+    entity.level.spawnParticles(`alexscaves:bio_pop`, false,
+        rX, rY + 0.25, rZ,
+        radius / 3, 0, radius / 3,
+        radius * 4, 0)
+    entity.level.spawnParticles(`alexscaves:big_splash_effect`,
+        false, rX, rY + 0.25, rZ,
+        radius / 3, 0, radius / 3,
+        radius * 8, 0)
+    entity.level.playSound(null, rX, rY, rZ, "alexscaves:mine_guardian_explode", "players", 2, Math.random() * (2 - 1.6) + 1.6)
 
-    let explosionRadius = 7.5
+    // explosion dmg/effect
+    let explosionRadius = radius
     entity.level
         // get enities within 7.5 block range cube I think
         .getEntitiesWithin(AABB.of(rX, rY, rZ, rX, rY, rZ).inflate(explosionRadius))
@@ -39,7 +52,7 @@ global.projHit = (context) => {
         // loop every tarr and hurt + apply weakness
         .forEach(/** @param {Internal.LivingEntity} entity */ entity => {
             entity.potionEffects.add("weakness", 200, 99, true, true)
-            entity.attack(entity.damageSources().generic(), 8)
+            entity.attack(entity.damageSources().generic(), damage)
         })
     // remove projectile entity after explosion
     entity.remove("discarded")
@@ -47,12 +60,31 @@ global.projHit = (context) => {
 
 // registering projectile + item
 StartupEvents.registry('entity_type', event => {
-    let proj = event.create('tarr_bomb', 'entityjs:projectile')
-    proj.renderOffset(0, 0, 0)
-    proj.renderScale(1, 1, 1)
-    proj.isAttackable(false)
-    proj.sized(1, 1)
-    proj.item(item => { item.canThrow(true) }) // add a throwable item for the projectile
-    proj.onHitBlock(context => { global.projHit(context) })
-    proj.onHitEntity(context => { global.projHit(context) })
+    let bombsArray = [
+        ['water_grenade', 4, 8],
+        ['water_bomb', 12, 16],
+        ['tsarr_bomba', 64, 50]
+    ]
+
+    for (let [bombName, bombRadius, bombDamage] of bombsArray) {
+        (function () {
+            let name = bombName
+            let radius = bombRadius
+            let damage = bombDamage
+
+            let proj = event.create(name, 'entityjs:projectile')
+            proj.textureLocation(entity => { return `kubejs:textures/entity/bombs/${name}.png` })
+            proj.renderOffset(0, 0, 0)
+            proj.renderScale(1, 1, 1)
+            proj.isAttackable(false)
+            proj.sized(1, 1)
+            proj.item(item => { // add a throwable item for the projectile
+                item.texture(`kubejs:item/bombs/${name}`)
+                item.tooltip(Text.of([`§8[§7 Purifies §cTarrs§7 in a §b${bombRadius}x${bombRadius}§7 area §8]`]))
+                item.canThrow(true)
+            })
+            proj.onHitBlock(context => { global.projHit(context, radius, damage) })
+            proj.onHitEntity(context => { global.projHit(context, radius, damage) })
+        })()
+    }
 })
